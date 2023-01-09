b33d8336bfd087f264689a0f5944ebc707ca4661
LucaFaedda <lucakarl91@gmail.com>
2023-01-09 15:44:37 +0100

    creato btn e stampato in console email

--- js/script.js
+++ js/script.js
@@ -5,8 +5,22 @@ const {
 createApp({
     data(){
         return{
-       
+            response: null,
         }
-    }
+    },
+    methods: {
+        createEmail(){
+            for(i = 1 ; i<11; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
+                    this.response = result.data.response
+                    console.log(this.response)
+                  
+                })
+
+            }
+
+        }
+    },
+  
 
 }).mount('#app')
