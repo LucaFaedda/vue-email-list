6aa8df1267b57f8428e3795ec585e4d687c11803
LucaFaedda <lucakarl91@gmail.com>
2023-01-09 16:00:32 +0100

    dinito esercizio

--- js/script.js
+++ js/script.js
@@ -6,19 +6,21 @@ createApp({
     data(){
         return{
             response: null,
+            emailArray: [],
         }
     },
     methods: {
         createEmail(){
-            for(i = 1 ; i<11; i++){
+            for(let i = 0 ; i<10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=>{
+                    this.emailArray.push(result.data.response)
                     this.response = result.data.response
-                    console.log(this.response)
-                  
                 })
-
+                
             }
-
+            console.log(this.emailArray)
+            
+            
         }
     },
   
